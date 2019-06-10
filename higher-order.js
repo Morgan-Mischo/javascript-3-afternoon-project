f4ec2a1b96bb7f7a069447f5dadfd4748ab71117
Morgan-Mischo <goramorgan@gmail.com>
2019-06-10 09:30:36 -0600

    Problems 5 and 6 on higher order.

--- higher-order.js
+++ higher-order.js
@@ -133,6 +133,15 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases
+.filter (e => 
+  {
+    return e .owner === "Bob"; 
+  })
+  .reduce((acc, curr) =>
+  {
+    return acc + curr.price
+  }, 0 ); 
+  console.log(bobsTotal); 
 
 
